2ae05aae8bb3e1ea2bf16cf92492d2f408072170
Fangv01 <578300640@qq.com>
2019-08-07 10:57:38 +0800

    合并message

--- Message/app/src/main/java/com/example/message/Fragment1.java
+++ Message/app/src/main/java/com/example/message/Fragment1.java
@@ -8,30 +8,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.message.message.Message_Box;
-import com.example.message.message.Message_Received;
 import com.example.message.message.Message_Send;
-import com.example.message.message.Message_Sended;
+import com.example.message.message.Received_Detail;
+import com.example.message.message.Received_Detail2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment1 extends Fragment {
     private View rootView;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.activity_message, container, false);
-        rootView.findViewById(R.id.received).setOnClickListener(new View.OnClickListener() {
+        rootView.findViewById(R.id.received1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent1=new Intent();
-                intent1.setClass(getActivity(), Message_Received.class);
+                intent1.setClass(getActivity(), Received_Detail.class);
                 startActivity(intent1);
             }
         });
-        rootView.findViewById(R.id.sended).setOnClickListener(new View.OnClickListener() {
+        rootView.findViewById(R.id.received2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2=new Intent();
-                intent2.setClass(getActivity(), Message_Sended.class);
-                startActivity(intent2);
+                Intent intent1=new Intent();
+                intent1.setClass(getActivity(), Received_Detail.class);
+                startActivity(intent1);
+            }
+        });
+        rootView.findViewById(R.id.received9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent();
+                intent1.setClass(getActivity(), Received_Detail2.class);
+                startActivity(intent1);
             }
         });
         rootView.findViewById(R.id.box).setOnClickListener(new View.OnClickListener() {
@@ -50,8 +62,10 @@ public class Fragment1 extends Fragment {
                 startActivity(intent4);
             }
         });
+
         return rootView;
     }
+
 }
 
 
