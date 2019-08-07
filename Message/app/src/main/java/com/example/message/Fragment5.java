a035813d6624b14cf1308a7f033e3a11cf6f042a
Fangv01 <578300640@qq.com>
2019-08-07 18:01:34 +0800

    MYYYYYYYYYYYYYYYYYYYYY

--- Message/app/src/main/java/com/example/message/Fragment5.java
+++ Message/app/src/main/java/com/example/message/Fragment5.java
@@ -16,20 +16,11 @@ public class Fragment5 extends Fragment {
     private View view;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         view=inflater.inflate(R.layout.activity_my,container,false);
-        Button md=view.findViewById(R.id.my_document);
-        Button ms=view.findViewById(R.id.setting);
-        md.setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.my_document).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i1=new Intent(getActivity(),My_Document.class);
                 startActivity(i1);
-            }
-        });
-        ms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i2=new Intent(getActivity(), Setting.class);
-                startActivity(i2);
             }
         });
         return view;
